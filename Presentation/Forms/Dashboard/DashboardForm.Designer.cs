5629737f521dd6a241b57e942630ade3481ad1f8
Enakhe <izuagbesam@gmail.om>
2024-07-27 16:04:58 +0100

    successfully edited the dashboard cards

--- Presentation/Forms/Dashboard/DashboardForm.Designer.cs
+++ Presentation/Forms/Dashboard/DashboardForm.Designer.cs
@@ -31,11 +31,23 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashboardForm));
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtRoomCount = new System.Windows.Forms.Label();
             this.btnView = new System.Windows.Forms.Button();
+            this.txtRoomCount = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.flowLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -50,53 +62,102 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(156)))), ((int)(((byte)(18)))));
+            this.panel1.Controls.Add(this.splitContainer1);
             this.panel1.Controls.Add(this.btnView);
-            this.panel1.Controls.Add(this.txtRoomCount);
-            this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(30, 30);
             this.panel1.Margin = new System.Windows.Forms.Padding(30);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(367, 178);
+            this.panel1.Size = new System.Drawing.Size(365, 150);
             this.panel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(18, 11);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 45);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Rooms";
-            // 
-            // txtRoomCount
-            // 
-            this.txtRoomCount.Font = new System.Drawing.Font("Segoe UI", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtRoomCount.ForeColor = System.Drawing.Color.White;
-            this.txtRoomCount.Location = new System.Drawing.Point(177, 52);
-            this.txtRoomCount.Name = "txtRoomCount";
-            this.txtRoomCount.Size = new System.Drawing.Size(187, 86);
-            this.txtRoomCount.TabIndex = 1;
-            this.txtRoomCount.Text = "0";
-            this.txtRoomCount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // btnView
             // 
-            this.btnView.BackColor = System.Drawing.Color.White;
+            this.btnView.BackColor = System.Drawing.Color.Black;
             this.btnView.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnView.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnView.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnView.FlatAppearance.BorderSize = 0;
             this.btnView.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnView.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnView.Location = new System.Drawing.Point(0, 141);
+            this.btnView.ForeColor = System.Drawing.Color.White;
+            this.btnView.Location = new System.Drawing.Point(0, 113);
             this.btnView.Name = "btnView";
-            this.btnView.Size = new System.Drawing.Size(367, 37);
+            this.btnView.Size = new System.Drawing.Size(365, 37);
             this.btnView.TabIndex = 2;
             this.btnView.Text = "Click to view rooms";
             this.btnView.UseVisualStyleBackColor = false;
             this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // txtRoomCount
+            // 
+            this.txtRoomCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtRoomCount.Font = new System.Drawing.Font("Segoe UI", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRoomCount.ForeColor = System.Drawing.Color.White;
+            this.txtRoomCount.Location = new System.Drawing.Point(0, 0);
+            this.txtRoomCount.Name = "txtRoomCount";
+            this.txtRoomCount.Size = new System.Drawing.Size(240, 113);
+            this.txtRoomCount.TabIndex = 1;
+            this.txtRoomCount.Text = "0";
+            this.txtRoomCount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Rooms";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtRoomCount);
+            this.splitContainer1.Size = new System.Drawing.Size(365, 113);
+            this.splitContainer1.SplitterDistance = 121;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer2.Size = new System.Drawing.Size(121, 113);
+            this.splitContainer2.SplitterDistance = 31;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // DashboardForm
             // 
@@ -112,7 +173,16 @@
             this.Load += new System.EventHandler(this.DashboardForm_Load);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -124,5 +194,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnView;
         private System.Windows.Forms.Label txtRoomCount;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Presentation/Forms/Home.Designer.cs
+++ Presentation/Forms/Home.Designer.cs
@@ -395,7 +395,6 @@
             this.reservationListToolStripMenuItem.Name = "reservationListToolStripMenuItem";
             this.reservationListToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.reservationListToolStripMenuItem.Text = "Reservation List";
-            this.reservationListToolStripMenuItem.Click += new System.EventHandler(this.reservationListToolStripMenuItem_Click);
             // 
             // roomCheckoutToolStripMenuItem
             // 
