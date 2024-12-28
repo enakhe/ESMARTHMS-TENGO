345ae7f82f94741897a9b9bde4f1dc662f703d3a
ENAKHE <izuagbesam@gmail.com>
2024-12-28 15:02:07 +0100

    Ready to create a new branch for accounting

---
+++ Presentation/Forms/Account/ChartOfAccount/ChartAccountForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ESMART_HMS.Presentation.Forms.Account.ChartOfAccount
+{
+    public partial class ChartAccountForm : Form
+    {
+        public ChartAccountForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
